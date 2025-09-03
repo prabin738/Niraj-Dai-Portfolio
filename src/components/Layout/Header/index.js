6e209ff7a36178db08495baa6e705a920b4aea0d
prabin738 <prabinsingh750@gmail.com>
2025-09-03 17:54:42 +0545

    basic tweaks done

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,150 +1,155 @@
-import React, { useState, useEffect } from 'react'; 
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import TopHeader from './TopBar';
-import MenuItems from './MenuItems';
-import MobileMenu from './MobileMenu';
+import TopHeader from "./TopBar";
+import MenuItems from "./MenuItems";
+import MobileMenu from "./MobileMenu";
 import navShape from "../../../assets/images/shape/shape-1.png";
 import navImg from "../../../assets/images/logo.png";
 import stickyImg from "../../../assets/images/logo.png";
 
-
 // import whiteLogo from '../../../assets/img/logo/white-logo.png';
 
 const Header = (props) => {
-	const { parentMenu, secondParentMenu, activeMenu,  } = props;
-	const [isVisible, setIsVisible] = useState(false);
+  const { parentMenu, secondParentMenu, activeMenu } = props;
+  const [isVisible, setIsVisible] = useState(false);
 
+  const [isMobileMenu, setMobileMenu] = useState(false);
+  const handleMobileMenu = () => {
+    setMobileMenu(!isMobileMenu);
+    !isMobileMenu
+      ? document.body.classList.add("mobile-menu-visible")
+      : document.body.classList.remove("mobile-menu-visible");
+  };
 
-	const [isMobileMenu, setMobileMenu] = useState(false)
-    const handleMobileMenu = () => {
-        setMobileMenu(!isMobileMenu)
-        !isMobileMenu ? document.body.classList.add("mobile-menu-visible") : document.body.classList.remove("mobile-menu-visible")
-    }
+  useEffect(() => {
+    // Sticky is displayed after scrolling for 100 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-	
+    window.addEventListener("scroll", toggleVisibility);
 
-	useEffect(() => {
-		// Sticky is displayed after scrolling for 100 pixels
-		const toggleVisibility = () => {
-			if (window.pageYOffset > 100) {
-				setIsVisible(true);
-			} else {
-				setIsVisible(false);
-			}
-		};
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
-		window.addEventListener("scroll", toggleVisibility);
-
-		return () => window.removeEventListener("scroll", toggleVisibility);
-	}, []);
-
-	
-
-	return (
-		<React.Fragment>
-			<>
-				{/* main header */}
-				<header className={`main-header ${isVisible ? "fixed-header" : ""}`}>
-					{/* header-top */}
-					<TopHeader />
-					{/* header-lower */}
-					<div className="header-lower">
-						<div className="outer-container">
-							<div className="outer-box">
-								<div
-									className="shape"
-									style={{ backgroundImage: `url(${navShape})` }}
-								/>
-								<div className="menu-area">
-									<div className="logo-box">
-										<figure className="logo">
-											<Link to="/">
-												<img src={navImg} alt="" />
-											</Link>
-										</figure>
-									</div>
-									{/*Mobile Navigation Toggler*/}
-									<div className="mobile-nav-toggler" onClick={handleMobileMenu}>
-										<i className="icon-bar"></i>
-										<i className="icon-bar"></i>
-										<i className="icon-bar"></i>
-									</div>
-									<nav className="main-menu navbar-expand-md navbar-light clearfix">
-										<div
-											className="collapse navbar-collapse show clearfix"
-											id="navbarSupportedContent"
-										>
-											<ul className="navigation clearfix">
-												<MenuItems
-													parentMenu={parentMenu}
-													secondParentMenu={secondParentMenu}
-													activeMenu={activeMenu}
-												/>
-											</ul>
-										</div>
-									</nav>
-								</div>
-								<div className="menu-right-content">
-									<div className="support-box">
-										<div className="icon-box gradient-color">
-											<i className="icon-2" />
-										</div>
-										<Link to="tel:2395432170108">(239)-543-217-0108</Link>
-									</div>
-									<div className="btn-box">
-										<Link to="/" className="theme-btn btn-one">
-											<span>Let’s Contact</span>
-										</Link>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					{/*sticky Header*/}
-					<div className="sticky-header">
-						<div className="outer-container">
-							<div className="outer-box">
-								<div className="menu-area">
-									<div className="logo-box">
-										<figure className="logo">
-											<Link to="/"><img src={stickyImg} alt="" /></Link>
-										</figure>
-									</div>
-									<nav className="main-menu clearfix">
-										<div className="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
-											<ul className="navigation clearfix">
-												<MenuItems
-													parentMenu={parentMenu}
-													secondParentMenu={secondParentMenu}
-													activeMenu={activeMenu}
-												/>
-											</ul>
-										</div>
-									</nav>
-								</div>
-								<div className="menu-right-content">
-									<div className="support-box">
-										<div className="icon-box gradient-color"><i className="icon-2"></i></div>
-										<Link to="tel:2395432170108">(239)-543-217-0108</Link>
-									</div>
-									<div className="btn-box">
-										<Link to="/" className="theme-btn btn-one"><span>Let’s Contact</span></Link>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					{/* End Sticky Menu */}
-					
-					<MobileMenu handleMobileMenu={handleMobileMenu} />
-					
-					
-				</header>
-				{/* main-header end */}
-			</>
-		</React.Fragment>
-	);
-}
+  return (
+    <React.Fragment>
+      <>
+        {/* main header */}
+        <header className={`main-header ${isVisible ? "fixed-header" : ""}`}>
+          {/* header-top */}
+          {/* <TopHeader /> */}
+          {/* header-lower */}
+          <div className="header-lower">
+            <div className="outer-container">
+              <div className="outer-box">
+                <div
+                  className="shape"
+                  style={{ backgroundImage: `url(${navShape})` }}
+                />
+                <div className="menu-area">
+                  <div className="logo-box">
+                    <figure className="logo">
+                      <Link to="/">
+                        <img src={navImg} alt="" />
+                      </Link>
+                    </figure>
+                  </div>
+                  {/*Mobile Navigation Toggler*/}
+                  <div
+                    className="mobile-nav-toggler"
+                    onClick={handleMobileMenu}
+                  >
+                    <i className="icon-bar"></i>
+                    <i className="icon-bar"></i>
+                    <i className="icon-bar"></i>
+                  </div>
+                  <nav className="main-menu navbar-expand-md navbar-light clearfix">
+                    <div
+                      className="collapse navbar-collapse show clearfix"
+                      id="navbarSupportedContent"
+                    >
+                      <ul className="navigation clearfix">
+                        <MenuItems
+                          parentMenu={parentMenu}
+                          secondParentMenu={secondParentMenu}
+                          activeMenu={activeMenu}
+                        />
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+                <div className="menu-right-content">
+                  <div className="support-box">
+                    <div className="icon-box gradient-color">
+                      <i className="icon-2" />
+                    </div>
+                    <Link to="tel:2395432170108">(239)-543-217-0108</Link>
+                  </div>
+                  <div className="btn-box">
+                    <Link to="/" className="theme-btn btn-one">
+                      <span>Let’s Contact</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*sticky Header*/}
+          <div className="sticky-header">
+            <div className="outer-container">
+              <div className="outer-box">
+                <div className="menu-area">
+                  <div className="logo-box">
+                    <figure className="logo">
+                      <Link to="/">
+                        <img src={stickyImg} alt="" />
+                      </Link>
+                    </figure>
+                  </div>
+                  <nav className="main-menu clearfix">
+                    <div
+                      className="collapse navbar-collapse show clearfix"
+                      id="navbarSupportedContent"
+                    >
+                      <ul className="navigation clearfix">
+                        <MenuItems
+                          parentMenu={parentMenu}
+                          secondParentMenu={secondParentMenu}
+                          activeMenu={activeMenu}
+                        />
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+                <div className="menu-right-content">
+                  <div className="support-box">
+                    <div className="icon-box gradient-color">
+                      <i className="icon-2"></i>
+                    </div>
+                    <Link to="tel:2395432170108">(239)-543-217-0108</Link>
+                  </div>
+                  <div className="btn-box">
+                    <Link to="/" className="theme-btn btn-one">
+                      <span>Let’s Contact</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End Sticky Menu */}
+          <MobileMenu handleMobileMenu={handleMobileMenu} />
+        </header>
+        {/* main-header end */}
+      </>
+    </React.Fragment>
+  );
+};
 
 export default Header;
--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -1,17 +1,14 @@
-
 // import Swiper core and required modules
-import { Link } from 'react-router-dom';
-import { Navigation, Autoplay, Pagination, } from 'swiper/modules';
+import { Link } from "react-router-dom";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 import bgImg1 from "../../assets/images/banner/banner-1.jpg";
 import bgImg2 from "../../assets/images/banner/banner-3.jpg";
@@ -19,84 +16,118 @@ import bgShape1 from "../../assets/images/shape/shape-2.png";
 import bgShape2 from "../../assets/images/shape/shape-3.png";
 import bgShape3 from "../../assets/images/shape/shape-4.png";
 
-
 const swiperOptions = {
-    modules: [Autoplay, Pagination, Navigation],
-    slidesPerView: 1,
-    spaceBetween: 0,
-    autoplay: {
+  modules: [Autoplay, Pagination, Navigation],
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
     delay: 7000,
     disableOnInteraction: false,
-    },
-    loop: true,
+  },
+  loop: true,
 
-    // Navigation
-    navigation: {
-        nextEl: '.h1n',
-        prevEl: '.h1p',
-    },
+  // Navigation
+  navigation: {
+    nextEl: ".h1n",
+    prevEl: ".h1p",
+  },
 
-    // Pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-
-
-}
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+};
 function Banner() {
   return (
     <>
-        <section className="banner-section p_relative">
-            <Swiper {...swiperOptions} className="banner-carousel">                    
-                <SwiperSlide className="slide-item p_relative">
-                <div className="pattern-layer"> 
-                    <div className="pattern-1" style={{backgroundImage:`url(${bgShape1})`}}></div>
-                    <div className="pattern-2" style={{backgroundImage:`url(${bgShape2})`}}></div>
-                    <div className="pattern-3" style={{backgroundImage:`url(${bgShape3})`}}></div>
+      <section className="banner-section p_relative">
+        <Swiper {...swiperOptions} className="banner-carousel">
+          <SwiperSlide className="slide-item p_relative">
+            <div className="pattern-layer">
+              <div
+                className="pattern-1"
+                style={{ backgroundImage: `url(${bgShape1})` }}
+              ></div>
+              <div
+                className="pattern-2"
+                style={{ backgroundImage: `url(${bgShape2})` }}
+              ></div>
+              <div
+                className="pattern-3"
+                style={{ backgroundImage: `url(${bgShape3})` }}
+              ></div>
+            </div>
+            <div
+              className="bg-layer"
+              style={{ backgroundImage: `url(${bgImg1})` }}
+            ></div>
+            <div className="auto-container">
+              <div className="content-box p_relative d_block z_5">
+                <h3>Niraj Thapa </h3>
+                <h2> Visual Storyteller</h2>
+                <p>
+                  A passionate individual with a deep enthusiasm for blending
+                  creativity and precision as a professional videographer and
+                  photographer.
+                </p>
+                <div className="btn-box">
+                  <Link
+                    to="https://www.instagram.com/nrz.thapa/"
+                    className="theme-btn btn-one"
+                  >
+                    <span>Discover More</span>
+                  </Link>
                 </div>
-                <div className="bg-layer" style={{backgroundImage:`url(${bgImg1})`}}></div>
-                <div className="auto-container">
-                    <div className="content-box p_relative d_block z_5">
-                        <h3>Discover The Truth</h3>
-                        <h2>Private Investigation Services</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit  ridiculus tellus elit turpis.</p>
-                        <div className="btn-box">
-                            <Link to="/" className="theme-btn btn-one"><span>Discover More</span></Link>
-                        </div>
-                    </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="slide-item p_relative">
+            <div className="pattern-layer">
+              <div
+                className="pattern-1"
+                style={{ backgroundImage: `url(${bgShape1})` }}
+              ></div>
+              <div
+                className="pattern-2"
+                style={{ backgroundImage: `url(${bgShape2})` }}
+              ></div>
+              <div
+                className="pattern-3"
+                style={{ backgroundImage: `url(${bgShape3})` }}
+              ></div>
+            </div>
+            <div
+              className="bg-layer"
+              style={{ backgroundImage: `url(${bgImg2})` }}
+            ></div>
+            <div className="auto-container">
+              <div className="content-box p_relative d_block z_5">
+                <h3>Discover The Truth</h3>
+                <h2>Private Investigation Services</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  ridiculus tellus elit turpis.
+                </p>
+                <div className="btn-box">
+                  <Link to="/" className="theme-btn btn-one">
+                    <span>Discover More</span>
+                  </Link>
                 </div>
-                </SwiperSlide>
-                <SwiperSlide className="slide-item p_relative">
-                        <div className="pattern-layer"> 
-                        <div className="pattern-1" style={{backgroundImage:`url(${bgShape1})`}}></div>
-                        <div className="pattern-2" style={{backgroundImage:`url(${bgShape2})`}}></div>
-                        <div className="pattern-3" style={{backgroundImage:`url(${bgShape3})`}}></div>
-                    </div>
-                    <div className="bg-layer" style={{backgroundImage:`url(${bgImg2})`}}></div>
-                    <div className="auto-container">
-                        <div className="content-box p_relative d_block z_5">
-                            <h3>Discover The Truth</h3>
-                            <h2>Private Investigation Services</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit  ridiculus tellus elit turpis.</p>
-                            <div className="btn-box">
-                                <Link to="/" className="theme-btn btn-one"><span>Discover More</span></Link>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                
-                <div className="owl-nav">
-                    <button type="button" className="owl-prev h1p">
-                        <span className=""></span>
-                    </button>
-                    <button type="button" className="owl-next h1n">
-                        <span className=""></span>
-                    </button>
-                </div>
-            </Swiper>
-        </section>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <div className="owl-nav">
+            <button type="button" className="owl-prev h1p">
+              <span className=""></span>
+            </button>
+            <button type="button" className="owl-next h1n">
+              <span className=""></span>
+            </button>
+          </div>
+        </Swiper>
+      </section>
     </>
   );
 }
